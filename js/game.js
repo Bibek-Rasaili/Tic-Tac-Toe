872edc8cc2c95bdea3ba9b5c62e86929219abe24
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-10 23:56:02 +0000

    call checkWon function after player 1's 3rd turn, where win conditions start

--- js/game.js
+++ js/game.js
@@ -2,13 +2,23 @@ var gamePattern = [];
 var turnO = true;
 
 var turnOcount = 0;
-
-
+//documents/tracks the amout of turn O (/player1) has had.
+//This will be used to trigger check won when its 3;
 
 
 function checkWon(){
 
+  alert("Player 1's 3rd turn has triggered me");
+
+
+
+
+
+
 }
+
+
+
 
 function resetGame(){
   $('div[type="button"]').removeClass("checked");
@@ -47,12 +57,25 @@ $('div[type="button"]').click(function() {
 
 //if winning combination, win/game over!
 // GAME ALGORITHM
-  var won = checkWon();
+  var won = false;
+  //if turnOcount === 3 then winPossible = true, if (winPossible) then run...
+  //or just have if turnOcount >= 3 then run...
+
+  if (turnOcount >= 3)
+    won = checkWon(); //under dev
+
+
+
+
 
   if (won)
   {
     alert("Congratulations, you won!");
     setTimeout(resetGame,2000);
+    //restGame()
+
+    //Also Remember to RESET/initialise the variable
+    //such as turnO, turnOcount, etc
   }
 //Because its player 1 and 2,
 // THERE will be no such thing as "GAME OVER".
