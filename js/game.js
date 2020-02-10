23238f73b16f8397d59035032133ee2dba4a2b6c
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-10 22:36:24 +0000

    Adding symbols on clicking (the box)

--- js/game.js
+++ js/game.js
@@ -2,7 +2,17 @@
 var gamePattern = [];
 
 
+function addSymbol(id) {
+  $('#'+id).append("<img src='images/circle.png' alt='circle'>");
+}
+
 //Add EventListener to boxes
+$('div[type="button"]').click(function(){
+  addSymbol(this.id);
+
+});
+
+
 
 //when called add a symbol, (If not already - check)
 
