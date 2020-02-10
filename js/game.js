8dd5ecce508692f9aca4662d59b511e20d916850
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-10 23:39:14 +0000

    Implementing Reset Game

--- js/game.js
+++ js/game.js
@@ -1,11 +1,29 @@
 var gamePattern = [];
 var turnO = true;
 
+var turnOcount = 0;
+
+
+
+
+function checkWon(){
+
+}
+
+function resetGame(){
+  $('div[type="button"]').removeClass("checked");
+
+  $('img').remove();
+  //reset the game by removing the images and "checked" class from the divs/box(es)
+}
 
 function addSymbol(id) {
   if (turnO) {
     $('#' + id).append("<img src='images/circle1.png' alt='circle'>");
       $('#'+id).addClass("checked");
+
+      turnOcount++;//because O goes first
+      //first winning condition possible is when O is on 3rd turn.
   } else {
     $('#' + id).append("<img src='images/cross.png' alt='cross'>");
       $('#'+id).addClass("checked");
@@ -24,8 +42,26 @@ $('div[type="button"]').click(function() {
   else
     alert("Soz it has been checked, love");
 //when called adding a symbol, (check If not already  )
-});
+
 
 
 //if winning combination, win/game over!
 // GAME ALGORITHM
+  var won = checkWon();
+
+  if (won)
+  {
+    alert("Congratulations, you won!");
+    setTimeout(resetGame,2000);
+  }
+//Because its player 1 and 2,
+// THERE will be no such thing as "GAME OVER".
+
+//If it was against comp/algorithm, we can have IF statement to check who won
+// and have else statement here to process "Game Over" when or if required.
+});
+
+
+  $('#reset').click(function(){
+    resetGame();
+  });
