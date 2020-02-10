76997d5b413d08341d5fb3a1ec15a3c218f6d094
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-10 22:51:51 +0000

    adding classes to the checked boxes to later implement anti-collision

--- js/game.js
+++ js/game.js
@@ -1,27 +1,30 @@
-
 var gamePattern = [];
 var turnO = true;
 
 
 function addSymbol(id) {
-  if (turnO)
-    $('#'+id).append("<img src='images/circle.png' alt='circle'>");
-  else
-    $('#'+id).append("<img src='images/cross.png' alt='cross'>");
+  if (turnO) {
+    $('#' + id).append("<img src='images/circle1.png' alt='circle'>");
+      $('#'+id).addClass("checked");
+  } else {
+    $('#' + id).append("<img src='images/cross.png' alt='cross'>");
+      $('#'+id).addClass("checked");
+  }
 
   turnO = !turnO; //so if its true, its set to false, if false, set to true.
   //this toggles the boolean to implement player turns
 }
 
 //Add EventListener to boxes
-$('div[type="button"]').click(function(){
+$('div[type="button"]').click(function() {
   addSymbol(this.id);
+
 
 });
 
 
 
-//when called add a symbol, (If not already - check)
+//when called adding a symbol, (check If not already  )
 
 //Next click should have different symbol (a boolean to implement turns)
 
