6441256f2b91c6f0116a820d97bb64079e2988ab
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-10 22:27:14 +0000

    Base/ Board for the game set up

---
+++ js/game.js
@@ -0,0 +1,12 @@
+
+var gamePattern = [];
+
+
+//Add EventListener to boxes
+
+//when called add a symbol, (If not already - check)
+
+//Next click should have different symbol (a boolean to implement turns)
+
+
+//if winning combination, win/game over!
