313ce39e43bd763e22d91960f5e11be4a9129ef3
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-11 00:01:43 +0000

    Don't check checkWon when user clicks a 'checked' box.

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ var turnOcount = 0;
 //This will be used to trigger check won when its 3;
 
 
-function checkWon(){
+function checkWon() {
 
   alert("Player 1's 3rd turn has triggered me");
 
@@ -20,23 +20,27 @@ function checkWon(){
 
 
 
-function resetGame(){
+function resetGame() {
   $('div[type="button"]').removeClass("checked");
 
   $('img').remove();
   //reset the game by removing the images and "checked" class from the divs/box(es)
+  turnO = true;
+  turnOcount = 0;
+  //Game finished.
+  //player 1 goes first. and 0 boxes checked.
 }
 
 function addSymbol(id) {
   if (turnO) {
     $('#' + id).append("<img src='images/circle1.png' alt='circle'>");
-      $('#'+id).addClass("checked");
+    $('#' + id).addClass("checked");
 
-      turnOcount++;//because O goes first
-      //first winning condition possible is when O is on 3rd turn.
+    turnOcount++; //because O goes first
+    //first winning condition possible is when O is on 3rd turn.
   } else {
     $('#' + id).append("<img src='images/cross.png' alt='cross'>");
-      $('#'+id).addClass("checked");
+    $('#' + id).addClass("checked");
   }
   //Next click should have different symbol (a boolean to implement turns)
 
@@ -47,44 +51,51 @@ function addSymbol(id) {
 //Add EventListener to boxes
 $('div[type="button"]').click(function() {
 
-  if(!$('#'+this.id).hasClass("checked"))
+  if (!$('#' + this.id).hasClass("checked")) {
     addSymbol(this.id);
-  else
+
+
+
+
+      //if winning combination, win/game over!
+      // GAME ALGORITHM
+      var won = false;
+      //if turnOcount === 3 then winPossible = true, if (winPossible) then run...
+      //or just have if turnOcount >= 3 then run...
+
+      if (turnOcount >= 3)
+        won = checkWon(); //under dev
+
+
+
+
+
+      if (won) {
+        alert("Congratulations, you won!");
+        setTimeout(resetGame, 2000);
+        //restGame()
+
+        //Also Remember to RESET/initialise the variable
+        //such as turnO, turnOcount, etc
+      }
+
+
+
+
+  } else {
     alert("Soz it has been checked, love");
-//when called adding a symbol, (check If not already  )
-
-
-
-//if winning combination, win/game over!
-// GAME ALGORITHM
-  var won = false;
-  //if turnOcount === 3 then winPossible = true, if (winPossible) then run...
-  //or just have if turnOcount >= 3 then run...
-
-  if (turnOcount >= 3)
-    won = checkWon(); //under dev
-
-
-
-
-
-  if (won)
-  {
-    alert("Congratulations, you won!");
-    setTimeout(resetGame,2000);
-    //restGame()
-
-    //Also Remember to RESET/initialise the variable
-    //such as turnO, turnOcount, etc
+    //when called adding a symbol, (check If not already  )
   }
-//Because its player 1 and 2,
-// THERE will be no such thing as "GAME OVER".
 
-//If it was against comp/algorithm, we can have IF statement to check who won
-// and have else statement here to process "Game Over" when or if required.
+
+  //Because its player 1 and 2,
+  // THERE will be no such thing as "GAME OVER".
+
+  //If it was against comp/algorithm, we can have IF statement to check who won
+  // and have else statement here to process "Game Over" when or if required.
 });
 
 
-  $('#reset').click(function(){
-    resetGame();
-  });
+$('#reset').click(function() {
+  resetGame();
+});
