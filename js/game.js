5c71239f7ff91a72d9b5d2fbe596b20937cd8590
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-12 11:00:16 +0000

    Validation working, refactoring needed

--- js/game.js
+++ js/game.js
@@ -5,22 +5,12 @@ var winPatterns = [
   [3, 4, 5], //3
   [6, 7, 8] // 6 (4)
 ];
-//See Algorithm Pattern image/plan to understand
-
 
 var turnO = true;
 
 var turnOcount = 0;
-//documents/tracks the amout of turn O (/player1) has had.
-//This will be used to trigger check won when its 3;
+//This will be used to trigger check won when its 3; Os 3rd turn
 
-// var board = [
-//   [-1, -1, -1],
-//   [-1, -1, -1],
-//   [-1, -1, -1]
-// ];
-//Decided to use 1D array instead of 2D and it doable with either
-//1D array is faster and consumes less memory as it require less allocation
 //0,  1,  2,
 var board = [-1, -1, -1,
   //3,  4,  5,
@@ -32,238 +22,157 @@ var board = [-1, -1, -1,
 //0 will signify O
 //1 will signify X
 
+var hasWinner = false;
+var winnerSymbol = -1; // set to 0 if O wins
+//set to 1 if X wins
+
+
+function checkFor0() {
+
+  if (board[0] === board[1] && board[1] === board[2] && !(board[0] === -1)) {
+    hasWinner = true;
+    //won.
+    if (board[0] === 0)
+      winnerSymbol = 0;
+    else
+      winnerSymbol = 1;
+    //can use else here as someone has won, so its 1 or the Other...
+  }
+  //____
+  if (board[0] === board[4] && board[4] === board[8] && !(board[0] === -1)) {
+    hasWinner = true;
+    //won.
+    if (board[0] === 0)
+      winnerSymbol = 0;
+    else
+      winnerSymbol = 1;
+    //can use else here as someone has won, so its 1 or the Other...
+  }
+  //___
+  if (board[0] === board[3] && board[3] === board[6] && !(board[0] === -1)) {
+    hasWinner = true;
+    //won.
+    if (board[0] === 0)
+      winnerSymbol = 0;
+    else
+      winnerSymbol = 1;
+    //can use else here as someone has won, so its 1 or the Other...
+  }
+  console.log("Checks for 0.. complete." + hasWinner + " " + winnerSymbol);
+}
+
+
+
+function checkFor2() {
+  //2
+  if (board[2] === board[4] && board[4] === board[6] && !(board[2] === -1)) {
+    hasWinner = true;
+    //won.
+    if (board[2] === 0)
+      winnerSymbol = 0;
+    else
+      winnerSymbol = 1;
+    //can use else here as someone has won, so its 1 or the Other...
+  }
+  //_________
+  if (board[2] === board[5] && board[5] === board[8] && !(board[2] === -1)) {
+    hasWinner = true;
+    //won.
+    if (board[2] === 0)
+      winnerSymbol = 0;
+    else
+      winnerSymbol = 1;
+    //can use else here as someone has won, so its 1 or the Other...
+  }
+  console.log("Checks for 2.. complete." + hasWinner + " " + winnerSymbol);
+}
+
+
+function checkFor136(a,b,c) {
+  //1
+  if (board[a] === board[b] && board[b] === board[c] && !(board[a] === -1)) {
+    hasWinner = true;
+    //won.
+    if (board[a] === 0)
+      winnerSymbol = 0;
+    else
+      winnerSymbol = 1;
+    //can use else here as someone has won, so its 1 or the Other...
+  }
+  console.log("Checks for 1.. complete." + hasWinner + " " + winnerSymbol);
+}
+//147
+//678
+//345
+
+
+
+function hasWon() {
+  console.log("hasWinner " + hasWinner);
+
+  if (!(board[0] === -1)){
+    checkFor0();
+  }
+
+  console.log("hasWinner" + hasWinner);
+
+  if (!(board[1] === -1) && hasWinner == false) {
+    console.log((hasWinner));
+    checkFor136(1,4,7);
+  }
+  if (!(board[2] === -1) && hasWinner == false) {
+    checkFor2();
+  }
+
+  if (!(board[3] === -1) && hasWinner == false) {
+    checkFor136(3,4,5);
+  }
+
+  if (!(board[6] === -1) && hasWinner == false) {
+    checkFor136(6,7,8);
+  }
+
+
+
+  //return false if no one won;
+  if (winnerSymbol === 0)
+    return alert("Player 0 has won");
+  if (winnerSymbol === 1)
+    return alert("Player X has won")
+  //cant be else cuz its show X won if O not true.
+
+  console.log("hasWinner" + hasWinner);
+  if (hasWinner) {
+    alert("Congratulations");
+    resetGame();
+  }
+}
+
+
+
+
+
 function mapArray() {
-  //if box class = checked
-  // check what class it has. - circle = 0, cross = 1
   for (var i = 0; i < 9; i++) {
+
     if ($('#cell' + (i + 1)).hasClass("checked")) {
+
+      if ($('#cell' + (i + 1)).hasClass("circle")) {
+        board[i] = 0;
+      } else {
+        board[i] = 1;
+      }
       //if cell1, has checked,
       // .hasClass circle, Board[1] = 0
       //else Board[1] = 1;
-
-      if ($('#cell' + (i + 1)).hasClass("circle")) {
-        console.log("I have class circle: " + $('#cell' + (i + 1)).hasClass("circle"));
-        board[i] = 0;
-      } else {
-        console.log("I have class cross: " + $('#cell' + (i + 1)).hasClass("cross"));
-        board[i] = 1;
-      }
-
       console.log(board);
-
-    } else {
-      //its empty, so do nothing.
-      //REDUNDANT
     }
   }
-  //if box class doesn't have class "checked", do nothing, as its empty.
-
   //no return required as board is global variable for now
-
-  //IMPORTANT
-  //REMEMBER TO INITIALISE board ARRAY
-  //in RESTGAME Function (basically once game is over/finished).
+  //REMEMBER TO INITIALISE board ARRAY in resetGame()
 }
-
-function hasWon() {
-  //would take in array as param if not global.
-  console.log("in hasWon");
-  //MAIN Algorithm here!!
-  var hasWinner = false; //start of assuming no one as hasWon
-  //then set to true if pattern exist.
-
-  //If not -1, then run through the Checks/pattern - DEPENDING on the location.
-
-  var winnerSymbol = -1; // set to 0 if O wins
-  //set to 1 if X wins
-  var same = 0;
-  //Algorithm Pattern check plan
-
-  for (var i = 0; i <= 3; i++) {
-    if (board[i] > -1) {
-      console.log("I am board cell no. " + (i + 1) + " and I am not empty, I have: " + board[i]);
-
-      var symbolCheck = 0; //0s can be replaced by this.
-
-      if(hasWinner)
-      {
-        break; //return true at hasWinner - in the win conditions
-              //IMPORTANT
-              // put the winnerSymbol on BEFORE the - return hasWinner tho!!!!!
-      }
-
-      //Done third
-      if (i === 0) {
-        //checks for 0
-      }
-
-      if (i === 1) {
-        //checks for 1
-        if (board[i] === symbolCheck) {
-          //check for O
-          same++;
-          if (board[4] === symbolCheck) {
-            same++;
-            if (board[7] === symbolCheck) {
-              same++;
-              //WIN
-              winnerSymbol = symbolCheck;
-              hasWinner = true;
-              // return hasWinner;
-
-            } else {
-              //No win - failed at 3rd/last spot
-              same = 0;
-              hasWinner = false;
-              winnerSymbol = -1;
-            }
-          } else {
-            //failed at 2nd spot
-            same = 0;
-          }
-        } else {// check for X
-          //symbolCheck is 0 right now.
-          symbolCheck++;
-          if (board[i] === 1) {
-            same++;
-            if (board[4] === 1) {
-              same++;
-              if (board[7] === 1) {
-                same++;
-                //WIN
-                hasWinner = true;
-                winnerSymbol = 1;
-                //symbolCheck = 0; not necessary here has after win,
-                // it should call RESET function
-              } else {
-                //no win - failed at 3rd spot.
-                same = 0;
-                winnerSymbol = -1;
-                hasWinner = false;
-                symbolCheck = 0;
-              }
-            } else {
-              //failed at 2nd Spot
-              same = 0;
-              symbolCheck = 0;
-              //Since it failed. this will allow to check for O
-              //first again.
-            }
-
-          } else {
-            console.log("This should never run!");
-            symbolCheck = 0;// this should never run
-//as its in else statement added for extra security.
-          }
-
-        }
-      }
-
-      if (i === 2) {
-        //checks for 2
-      }
-
-      if (i === 3) {
-        //checks for 3
-      }
-    }
-  }
-
-  if (board[6] > -1) { //Done first
-    console.log("I am board cell no. " + (i + 1) + " and I am not empty, I have: " + board[i]);
-
-    if (board[6] === 0) {
-      //check for O
-      same++;
-      if (board[7] === 0) {
-        same++;
-        if (board[8] === 0) {
-          //if won,
-          same++;
-          hasWinner = true;
-          winnerSymbol = 0;
-        } else {
-          //if 3rd one don't match, reset all/these
-          same = 0;
-          hasWinner = false;
-          winnerSymbol = -1;
-        }
-      }
-    } else //Done second
-    {
-      //check for 1
-      for (var i = 6; i <= 8; i++) {
-
-        if (board[i] === 1) {
-          same++; //will increment at 6, 7 and 8 if matched.
-
-          if (i === winPatterns[4][2]) { //i=8
-            if (same === 3) { //if matched at all 3, WIN
-
-              hasWinner = true;
-              winnerSymbol = 1;
-
-            } else {
-              //ELSE, this is the end of the road. RESET
-              same = 0;
-              hasWinner = false;
-              winnerSymbol = -1;
-
-            }
-          }
-        }
-
-
-
-        //____________
-        // Formulated from:
-        // if (board[6] === 1)
-        //   same++;
-        // if (board[7] === 1)
-        //   same++;
-        // if (board[8] === 1)
-        //   same++;
-        //   hasWinner = true;
-        //   winnerSymbol = 1;
-        // else
-        //   same=0;
-        //   hasWinner = false;
-        //   winnerSymbol = -1;
-        //_____________
-      }
-
-
-
-    }
-    //run pattern checks here
-  }
-  //if board[0] != -1
-  // then run checks
-
-  //if board[1] != -1
-  // run checks
-
-  //if board[2,3,6] != -1
-  // check
-
-
-
-
-
-
-
-
-
-  //returns true if won,
-  //false if not.
-}
-
 
 function checkWon() {
-
-  var checkSum = 0; //when checking if 0 won
-  //1 when checking if X won
-
   alert("Player 1's 3rd turn has triggered me");
   console.log(board);
 
@@ -271,20 +180,7 @@ function checkWon() {
   mapArray(); //board array is global, therefore don't need to assign this.
   // map the board into a 1D array
 
-
   hasWon();
-  // return hasWon(); THIS WILL BE IMPLEMENTED
-  //check for O wins
-  //check for X wins
-  //return true if someone wins
-
-  //return that with also soemthing ...(TBA)
-  //OR
-  //return or set a global variable like
-  // var Owon = -1; to 0 is O won
-  // Owon to 1; if X won..
-
-  //return false if no one won;
 }
 
 
@@ -299,7 +195,16 @@ function resetGame() {
   turnOcount = 0;
   //Game finished.
   //player 1 goes first. and 0 boxes checked.
+  hasWinner = false;
+  winnerSymbol = -1;
+
+  console.log(board);
+  for (var i = 0; i < board.length; i++) {
+    board[i] = -1;
+  }
+  console.log("reset board " + board);
 }
+
 
 function addSymbol(id) {
   if (turnO) {
@@ -318,54 +223,34 @@ function addSymbol(id) {
   //this toggles the boolean to implement player turns
 }
 
+
 //Add EventListener to boxes
 $('div[type="button"]').click(function() {
 
   if (!$('#' + this.id).hasClass("checked")) {
     addSymbol(this.id);
 
-
-
-
-    //if winning combination, win/game over!
-    // GAME ALGORITHM
     var won = false;
-    //if turnOcount === 3 then winPossible = true, if (winPossible) then run...
-    //or just have if turnOcount >= 3 then run...
 
-    if (turnOcount >= 3)
-      won = checkWon(); //under dev
-
-
-
+    if (turnOcount >= 3) {
+      // won = checkWon(); //under dev
+      checkWon(); // FOR NOW.
+    }
 
 
     if (won) {
       alert("Congratulations, you won!");
       setTimeout(resetGame, 2000);
       //restGame()
-
-      //Also Remember to RESET/initialise the variable
-      //such as turnO, turnOcount, etc
+      //Also Remember to RESET/initialise the variable such as turnO, turnOcount, etc
     }
-
-
-
-
   } else {
     alert("Soz it has been checked, love");
-    //when called adding a symbol, (check If not already  )
+    //prevents adding symbol of checked
   }
-
-
-  //Because its player 1 and 2,
-  // THERE will be no such thing as "GAME OVER".
-
-  //If it was against comp/algorithm, we can have IF statement to check who won
-  // and have else statement here to process "Game Over" when or if required.
 });
 
-
+//Reset Button click listener
 $('#reset').click(function() {
   resetGame();
 });
